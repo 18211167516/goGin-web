5293c1fa0f4b643077eb29038cacc43553442af6
=baichonghua <18211167516@163.com>
2020-06-19 16:13:55 +0800

    add models

---
+++ App/models/db.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+    "log"
+    "fmt"
+
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+
+    "gintest/config"
+)
+
+var db *gorm.DB
+
+type Model struct {
+    ID int `gorm:"primary_key" json:"id"`
+    CreatedOn int `json:"created_on"`
+    ModifiedOn int `json:"modified_on"`
+}
+
+func init() {
+    var (
+        err error
+        dbType, dbName, user, password, host, tablePrefix string
+    )
+
+
+    dbType = "mysql"
+    dbName = config.MysqlName
+    user = config.MysqlUser
+    password = config.MysqlPassword
+    host = config.MysqlHost
+    tablePrefix = config.MysqlPrefix
+
+    db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", 
+        user, 
+        password, 
+        host, 
+        dbName))
+
+    if err != nil {
+        log.Println(err)
+    }
+
+    gorm.DefaultTableNameHandler = func (db *gorm.DB, defaultTableName string) string  {
+        return tablePrefix + defaultTableName;
+    }
+
+    db.SingularTable(true)
+    db.DB().SetMaxIdleConns(10)
+    db.DB().SetMaxOpenConns(100)
+}
+
+func CloseDB() {
+    defer db.Close()
+}
---
+++ routes/route.go
@@ -0,0 +1,29 @@
+package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"gintest/App/controllers"
+)
+
+func InitRouter() *gin.Engine{
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	apiv1 := r.Group("/api/v1")
+    {
+        //获取标签列表
+        apiv1.GET("/tags", controllers.GetTags)
+        //新建标签
+        apiv1.POST("/tags", controllers.AddTag)
+        //更新指定标签
+        apiv1.PUT("/tags/:id", controllers.EditTag)
+        //删除指定标签
+        apiv1.DELETE("/tags/:id", controllers.DeleteTag)
+    }
+
+	return r;
+}
